260a0e9b406c1d1c58f4efb5d53b7f063daf4158
Stefano Menotti <s.menotti@idscorporation.com>
2016-04-20 10:41:25 -0400

    test

--- SimpleModule2/src/main/java/Main.java
+++ SimpleModule2/src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("This is the main class!");
+        System.out.println("This is the main stefanobranch class!");
         System.out.println("It works!Modification on second branch");
         System.out.println("This modification has been done on second branch, directly to github. And now from IDE_MAIN_3");
 
